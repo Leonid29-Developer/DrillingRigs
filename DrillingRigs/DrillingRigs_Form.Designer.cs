5cb0b5439b39ed8a9df258073d93cb0a2d1f90ed
Leonid29 <job@leonid29-developer.ru>
2024-03-04 21:39:20 +0400

    Добавьте файлы проекта.

---
+++ DrillingRigs/DrillingRigs_Form.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace DrillingRigs
+{
+    partial class DrillingRigs_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DrillingRigs_Form));
+            this.DrillingRig1 = new System.Windows.Forms.PictureBox();
+            this.DrillingRig2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Back_Button = new System.Windows.Forms.PictureBox();
+            this.Table = new System.Windows.Forms.TableLayoutPanel();
+            this.Description = new System.Windows.Forms.Label();
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.Label_Text = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DrillingRig1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DrillingRig2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Back_Button)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrillingRig1
+            // 
+            this.DrillingRig1.BackgroundImage = global::DrillingRigs.Properties.Resources.Наземные_буровые_установки;
+            this.DrillingRig1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.DrillingRig1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DrillingRig1.Location = new System.Drawing.Point(39, 39);
+            this.DrillingRig1.Margin = new System.Windows.Forms.Padding(30);
+            this.DrillingRig1.Name = "DrillingRig1";
+            this.DrillingRig1.Size = new System.Drawing.Size(300, 300);
+            this.DrillingRig1.TabIndex = 0;
+            this.DrillingRig1.TabStop = false;
+            this.DrillingRig1.Click += new System.EventHandler(this.DrillingRig1_Click);
+            // 
+            // DrillingRig2
+            // 
+            this.DrillingRig2.BackgroundImage = global::DrillingRigs.Properties.Resources.Передвижные_буровые_установки;
+            this.DrillingRig2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.DrillingRig2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DrillingRig2.Location = new System.Drawing.Point(408, 39);
+            this.DrillingRig2.Margin = new System.Windows.Forms.Padding(30);
+            this.DrillingRig2.Name = "DrillingRig2";
+            this.DrillingRig2.Size = new System.Drawing.Size(300, 300);
+            this.DrillingRig2.TabIndex = 1;
+            this.DrillingRig2.TabStop = false;
+            this.DrillingRig2.Click += new System.EventHandler(this.DrillingRig2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.5F);
+            this.label1.Location = new System.Drawing.Point(39, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Наземные буровые установки";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.5F);
+            this.label2.Location = new System.Drawing.Point(408, 347);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Передвижные буровые установки";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Back_Button
+            // 
+            this.Back_Button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Back_Button.BackgroundImage")));
+            this.Back_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Back_Button.Location = new System.Drawing.Point(19, 458);
+            this.Back_Button.Margin = new System.Windows.Forms.Padding(10);
+            this.Back_Button.Name = "Back_Button";
+            this.Back_Button.Size = new System.Drawing.Size(50, 50);
+            this.Back_Button.TabIndex = 4;
+            this.Back_Button.TabStop = false;
+            this.Back_Button.Visible = false;
+            this.Back_Button.Click += new System.EventHandler(this.Back_Button_Click);
+            // 
+            // Table
+            // 
+            this.Table.ColumnCount = 1;
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.Table.Location = new System.Drawing.Point(320, 104);
+            this.Table.Margin = new System.Windows.Forms.Padding(10);
+            this.Table.Name = "Table";
+            this.Table.RowCount = 1;
+            this.Table.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.Table.Size = new System.Drawing.Size(408, 404);
+            this.Table.TabIndex = 5;
+            this.Table.Visible = false;
+            // 
+            // Description
+            // 
+            this.Description.AutoEllipsis = true;
+            this.Description.Font = new System.Drawing.Font("Times New Roman", 13.5F);
+            this.Description.Location = new System.Drawing.Point(316, 9);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(412, 85);
+            this.Description.TabIndex = 6;
+            this.Description.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Description.Visible = false;
+            // 
+            // Picture
+            // 
+            this.Picture.BackgroundImage = global::DrillingRigs.Properties.Resources.Наземные_буровые_установки;
+            this.Picture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Picture.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Picture.Location = new System.Drawing.Point(25, 25);
+            this.Picture.Margin = new System.Windows.Forms.Padding(30);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(275, 275);
+            this.Picture.TabIndex = 7;
+            this.Picture.TabStop = false;
+            this.Picture.Visible = false;
+            // 
+            // Label_Text
+            // 
+            this.Label_Text.Font = new System.Drawing.Font("Times New Roman", 13.5F);
+            this.Label_Text.Location = new System.Drawing.Point(25, 312);
+            this.Label_Text.Name = "Label_Text";
+            this.Label_Text.Size = new System.Drawing.Size(275, 35);
+            this.Label_Text.TabIndex = 8;
+            this.Label_Text.Text = "Наземные буровые установки";
+            this.Label_Text.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Label_Text.Visible = false;
+            // 
+            // DrillingRigs_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 527);
+            this.Controls.Add(this.Label_Text);
+            this.Controls.Add(this.Picture);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.Table);
+            this.Controls.Add(this.Back_Button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DrillingRig2);
+            this.Controls.Add(this.DrillingRig1);
+            this.Name = "DrillingRigs_Form";
+            ((System.ComponentModel.ISupportInitialize)(this.DrillingRig1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DrillingRig2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Back_Button)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DrillingRig1;
+        private System.Windows.Forms.PictureBox DrillingRig2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox Back_Button;
+        private System.Windows.Forms.TableLayoutPanel Table;
+        private System.Windows.Forms.Label Description;
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Label Label_Text;
+    }
+}
+
---
+++ DrillingRigs/DrillingRigs_Form.cs
@@ -0,0 +1,175 @@
+﻿using DrillingRigs.Properties;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace DrillingRigs
+{
+    public partial class DrillingRigs_Form : Form
+    {
+        public DrillingRigs_Form() => InitializeComponent();
+
+        private void ReActive(bool T)
+        {
+            DrillingRig1.Visible = T; label1.Visible = T; 
+            DrillingRig2.Visible = T;label2.Visible = T;
+            Picture.Visible = !T; Label_Text.Visible = !T;
+            Back_Button.Visible = !T; Table.Visible = !T;
+        }
+
+        private void DrillingRig1_Click(object sender, EventArgs e)
+        {
+            Picture.BackgroundImage = DrillingRig1.BackgroundImage; Label_Text.Text = label1.Text;
+            ReActive(false); TableFill_Stage1(label1.Text);
+        }
+
+        private void DrillingRig2_Click(object sender, EventArgs e)
+        {
+            Picture.BackgroundImage = DrillingRig2.BackgroundImage; Label_Text.Text = label2.Text;
+            ReActive(false); TableFill_Stage1(label2.Text); 
+        }
+
+        private void Back_Button_Click(object sender, EventArgs e)
+        { ReActive(true); Description.Visible = false; EXCEL.Quit(); }
+
+        private Excel.Application EXCEL { get; set; }
+        private Excel.Worksheet Sheet { get; set; }
+
+        private void TableFill_Stage1(string SheetName)
+        {
+            EXCEL = new Excel.Application();
+            //Excel.Workbook wkb = EXCEL.Workbooks.Open(@"C:\Users\leoni\Desktop\Новая папка (4)\3\БУ.xlsx");
+            Excel.Workbook wkb = EXCEL.Workbooks.Open($@"{Application.StartupPath}\Resources\БУ.xlsx");
+            Sheet = wkb.Sheets[SheetName] as Excel.Worksheet;
+            Table.Controls.Clear(); Description.Visible = false;
+
+            switch (SheetName)
+            {
+                case "Наземные буровые установки":
+                    {
+                        Excel.Range Ranged = Sheet.get_Range($"A:A"); int I1 = 0;
+                        if (Ranged != null) foreach (Excel.Range RName in Ranged)
+                            {
+                                if (I1 != 0) if (RName.Text == "") break;
+                                    else CreatePanel_Stage1(RName.Text, I1); I1++;
+                            }
+                    }
+                    break;
+                case "Передвижные буровые установки":
+                    {
+                        Excel.Range Ranged = Sheet.get_Range($"A:A"); int I1 = 0;
+                        if (Ranged != null) foreach (Excel.Range RName in Ranged)
+                            {
+                                if (I1 != 0) if (RName.Text == "") break;
+                                    else CreatePanel_Stage1(RName.Text, I1); I1++;
+                            }
+                    }
+                    break;
+            }
+
+            Table.AutoScroll = false; Table.HorizontalScroll.Enabled = false; Table.HorizontalScroll.Visible = false;
+            Table.AutoScroll = true;
+        }
+
+        private void TableFill_Stage2(object sender, EventArgs e)
+        {
+            var Element = (Label)sender; string[] Log = Element.Name.Split('_');
+            Table.Controls.Clear(); Description.Visible = true;
+
+            Excel.Range RangeHead = Sheet.get_Range($"A1:M1"); int I = 1;
+            Excel.Range Ranged = Sheet.get_Range($"A{Convert.ToInt32(Log[1])+1}: M{Convert.ToInt32(Log[1])+1}");
+            if (Ranged != null) foreach (Excel.Range RText in Ranged)
+                    if (RText.Text != "-" & RText.Text != "")
+                    {
+                        if (I == 1)
+                        {
+                            Picture.BackgroundImage = Resources.ResourceManager.GetObject(RText.Text/*.Replace(' ', '_')*/);
+                            Label_Text.Text = RText.Text;
+                        }
+                        if (I == 2) { Description.Text = RText.Text; }
+                        if (I > 2) CreatePanel_Stage2(RangeHead[I].Text, RText.Text);
+
+                        Table.AutoScroll = false; Table.HorizontalScroll.Enabled = false; 
+                        Table.HorizontalScroll.Visible = false; Table.AutoScroll = true;  I++;
+                    }
+        }
+
+        private void CreatePanel_Stage1(string Name, int Index)
+        {
+            Panel Main_Panel = new Panel
+            {
+                Name = $"RName_{Index}",
+                Size = new Size(Table.Width - 23, 50),
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+            {
+                // Наименование
+                Label Named = new Label
+                {
+                    Name = $"RName_{Index}",
+                    Text = Name,
+                    AutoSize = false,
+                    Size = new Size(Main_Panel.Width, Main_Panel.Height),
+                    Font = new Font("Times New Roman", 18),
+                    TextAlign = ContentAlignment.MiddleLeft
+                };
+
+                Named.Click += TableFill_Stage2; Main_Panel.Controls.Add(Named);
+            }
+
+            Table.Controls.Add(Main_Panel);
+        }
+
+        private void CreatePanel_Stage2(string HeadName, string Text)
+        {
+            Panel Main_Panel = new Panel
+            {
+                Size = new Size(Table.Width - 23, 100),
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+            {
+                // Заголовок
+                Label HeadNamed = new Label
+                {
+                    Text = HeadName,
+                    AutoSize = false,
+                    Size = new Size(Main_Panel.Width, Main_Panel.Height*2/3),
+                    Font = new Font("Times New Roman", 14),
+                    TextAlign = ContentAlignment.MiddleLeft
+                };
+
+                // Текст
+                Label Named = new Label
+                {
+                    Text = "➤ "+Text,
+                    AutoSize = false,
+                    Size = new Size(Main_Panel.Width, Main_Panel.Height/3),
+                    Font = new Font("Times New Roman", 14),
+                    TextAlign = ContentAlignment.MiddleLeft,
+                    Top = Main_Panel.Height * 2 / 3
+                };
+
+                Main_Panel.Controls.Add(HeadNamed); Main_Panel.Controls.Add(Named);
+            }
+
+            Table.Controls.Add(Main_Panel);
+        }
+    }
+
+    class Elements
+    {
+        public Control Control { get; private set; }
+        public Size Size { get; private set; }
+        public Point Location { get; private set; }
+
+        public Elements(Control NewControl, Size NewSize, Point NewLocation)
+        { Control = NewControl; Size = NewSize; Location = NewLocation; }
+
+        public void ResetPosition()
+        { Control.Size = Size; Control.Location = Location; Control.Visible = true; }
+
+        public void SetPosition(Size NewSize, Point NewLocation, Control DeActivate)
+        { Control.Size = NewSize; Control.Location = NewLocation; DeActivate.Visible = false; }
+    }
+}
